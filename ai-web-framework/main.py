b35e8bd30c92eed7aed1db4fc9ad0e9a713d6ddc
Raghu Kokku <raghu.kokku@ticketmaster.com>
2024-11-11 16:52:09 -0600

    initial-commit

---
+++ ai-web-framework/main.py
@@ -0,0 +1,48 @@
+# This is a sample Python script.
+import datetime
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import datetime
+import os
+from flask import Flask, jsonify
+from openai import OpenAI
+
+
+app = Flask(__name__)
+api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key = api_key)
+
+
+@app.route("/")
+def ping():
+    # Use a breakpoint in the code line below to debug your script.
+    return "Ping Ok !!! - " + str(datetime.datetime.now())
+
+
+@app.route("/countryList", methods = ['GET'])
+def country_list():
+    value = client.chat.completions.create(messages=[{'role': 'system', 'content': 'only country names in the world '
+                                                                                   'without any other text'}],
+                                           model='gpt-4o-mini')
+    choices = value.to_dict().get("choices")[0]
+    d = dict(choices)
+    data = {'countries' : dict(d.get('message')).get('content').split('\n')}
+    response = jsonify(data)
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    return response
+
+
+@app.route("/stateList", methods = ['GET'])
+def state_list():
+    value = client.chat.completions.create(messages=[{'role': 'system', 'content': 'only state names in the USA without'
+                                                                                   ' any other text '}],
+                                           model='gpt-4o-mini')
+    choices = value.to_dict().get("choices")[0]
+    d = dict(choices)
+    data = {'states' : dict(d.get('message')).get('content').split('\n')}
+    response = jsonify(data)
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    return response
+
